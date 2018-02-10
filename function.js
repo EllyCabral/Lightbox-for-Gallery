98fb3868b981f3a4517b2a2d8a2fd2aed133bc06
Testing <elly.c.cabral@gmail.com>
2018-02-09 16:29:29 -0800

    javascript addition

--- function.js
+++ function.js
@@ -19,5 +19,31 @@ $(document).ready(function($){
   });
   
   // handle the gallery
+  // first, how many images are there?
+  var num_img = $('.gal-wrap img').length;
+  // change the percentage
+  var percent = 100/num_img;
+  // apply it to the images
+  $('.gal-wrap img').css({
+     'width' : percent +'%'
+  });
+  // get the width of the gal-wrap div
+  var gal_wid = $('.gal-wrap').outerWidth();
+  console.log(gal_wid);
+  // multiply the width by the number of images
+  var ful_wid = num_img * gal_wid;
+  // set it
+  $('.gal-wrap').css({
+    'width' : ful_wid +'px'
+  });
   
+  // gallery navigation
+  $('.galnav.prev').click(function(){
+    // previous moves the last child to the front of the line (we're always viewing the first image)
+    $('.gal-wrap img:last-child').prependTo('.gal-wrap');
+  });
+  $('.galnav.next').click(function(){
+    // next moves the first child to the end of the line (we're always viewing the first image)
+    $('.gal-wrap img:first-child').appendTo('.gal-wrap');
+  });
 });
